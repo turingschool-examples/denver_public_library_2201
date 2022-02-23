8aa9e76c878f247703ab9a4eff8eb3a4c000a204
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-02-23 09:07:27 -0700

    Test: new tests for book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,25 @@
+require '.lib/book'
+require 'pry'
+
+
+RSpec.describe Book do
+
+  context 'create a book instance with attributes' do
+    book = Book.new({
+      author_first_name: "Harper",
+      author_last_name: "Lee",
+      title: "To Kill a Mockingbird",
+      publication_date: "July 11, 1960"
+      })
+
+    it 'exists' do
+      expect(book).to be_an_instance_of(Book)
+    end
+
+    it 'has attributes' do
+      expect(book.title).to eq("To Kill a Mockingbird")
+      expect(book.author).to eq("Harper Lee")
+      expect(book.publication_year).to eq("1960")
+    end
+  end
+end
