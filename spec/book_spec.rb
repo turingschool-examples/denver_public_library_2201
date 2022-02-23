d984deb4463958a05b44b6ce025f4bf1afb0e666
Rin Fabian <pfabianschool@gmail.com>
2022-02-23 10:33:17 -0600

    feat; wrote an initialize method for the book class that satisfies test conditions

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,11 +1,9 @@
 require './lib/book'
 
 RSpec.describe Book do
-  before (:each) do
-    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
-  end
 
   it 'can create a book' do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
     expect(book.title).to eq("To Kill a Mockingbird")
     expect(book.author).to eq("Harper Lee")
     expect(book.publication_year).to eq("1960")
