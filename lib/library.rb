c5100b7f6e8aca03223fa46d199de5034d376e9a
Sandisz Thieme <sandiszthieme@gmail.com>
2022-02-23 10:16:08 -0700

    Give Library ability to checkout books

--- lib/library.rb
+++ lib/library.rb
@@ -1,9 +1,10 @@
 class Library
-  attr_reader :name, :books, :authors
+  attr_reader :name, :books, :authors, :checked_out_books
   def initialize(name)
     @name = name
     @books = []
     @authors = []
+    @checked_out_books = []
   end
 
   def add_author(author)
@@ -14,14 +15,24 @@ class Library
 
   def publication_time_frame_for(author)
     timeframe = {}
+
     timeframe[:start] = (books.find { |book|
-      book.author == author.name
-    }).publication_year
+      book.author == author.name }).publication_year
 
     timeframe[:end] = (books.find_all { |book|
-      book.author == author.name
-    }[-1]).publication_year
+      book.author == author.name }[-1]).publication_year
 
     timeframe
+  end
+
+  def checkout(book)
+    if @checked_out_books.include?(book)
+      return false
+    elsif @books.include?(book)
+      @checked_out_books << book
+      return true
+    else
+      return false
+    end
   end
 end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -38,3 +38,31 @@ describe Library do
     expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
   end
 end
+
+describe Library do
+  dpl = Library.new("Denver Public Library")
+
+  charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+  villette = charlotte_bronte.write("Villette", "1853")
+  harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+  mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+  it "cannot checkout if the library doesn't have the book" do
+    expect(dpl.checkout(mockingbird)).to be false
+    expect(dpl.checkout(jane_eyre)).to be false
+  end
+
+  it "can checkout books it has" do
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    expect(dpl.checkout(jane_eyre)).to be true
+    expect(dpl.checked_out_books).to eq([jane_eyre])
+  end
+
+  it "cannot checkout checked out books" do
+    expect(dpl.checkout(jane_eyre)).to be false
+    expect(dpl.checkout(mockingbird)).to be true
+    expect(dpl.checkout(mockingbird)).to be false
+  end
+end
