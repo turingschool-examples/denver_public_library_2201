ac98569c2a6b140a00b5b009bd456ee2c48505e6
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-23 10:14:17 -0700

    Test: add tests for .publication_time_frame_for method

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -37,4 +37,18 @@ describe Library do
       expect(dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
     end
   end
+
+  describe 'publication_time_frame_for' do
+    it 'returns the oldest publication_year and the newest publication_year' do
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      professor = charlotte_bronte.write("The Professor", "1857")
+      villette = charlotte_bronte.write("Villette", "1853")
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+      dpl.add_author(charlotte_bronte)
+      dpl.add_author(harper_lee)
+
+      expect(dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start=>"1847", :end=>"1857"})
+      expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
+    end
+  end
 end
