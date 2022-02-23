2a3c57d726c88c2b43ed4d52e5dd2a3508a13572
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 09:37:06 -0700

    Test: author class can create instance of Book class

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe Author do
 
     expect(charlotte_bronte).to be_an_instance_of(Author)
   end
+
+  it "can return author name and books written as an empty array" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+    expect(charlotte_bronte.books).to eq([])
+  end
+
+  it "has class method 'write' which creates instance of Book and adds it to books array" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    expect(jane_eyre.class).to eq(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
+  end
 end
