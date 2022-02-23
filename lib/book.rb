d7ca01d9e9085c3d0d7617f683a08f3579dc5201
Sherman Au <shermau13@gmail.com>
2022-02-23 09:19:55 -0700

    feat: add author

---
+++ lib/book.rb
@@ -0,0 +1,16 @@
+class Book
+  attr_reader :title,
+              :author,
+              :publication_year
+
+  def initialize(details)
+    @author_first_name = details[:author_first_name]
+    @author_last_name = details[:author_last_name]
+    @title = details[:title]
+    @publication_date = details[:publication_date]
+  end
+
+  def author
+    "#{author_first_name} #{author_last_name}"
+  end
+end
