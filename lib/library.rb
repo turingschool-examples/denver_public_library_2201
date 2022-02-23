db7e0e938416b843304a4bfcdab5dcf6cb319593
Sandisz Thieme <sandiszthieme@gmail.com>
2022-02-23 10:04:37 -0700

    Give Library ability to add and interact with Authors and Books

--- lib/library.rb
+++ lib/library.rb
@@ -5,4 +5,23 @@ class Library
     @books = []
     @authors = []
   end
+
+  def add_author(author)
+    authors << author
+    @books << author.books.sort_by { |book| book.publication_year.to_i }
+    @books.flatten!
+  end
+
+  def publication_time_frame_for(author)
+    timeframe = {}
+    timeframe[:start] = (books.find { |book|
+      book.author == author.name
+    }).publication_year
+
+    timeframe[:end] = (books.find_all { |book|
+      book.author == author.name
+    }[-1]).publication_year
+
+    timeframe
+  end
 end
