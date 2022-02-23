f0f15187e57ef3e0e42c772df2b7395bf4793ff8
Rin Fabian <pfabianschool@gmail.com>
2022-02-23 10:40:48 -0600

    test; created tests for the author class

---
+++ spec/author_spec.rb
@@ -0,0 +1,28 @@
+require './lib/book'
+require './lib/author'
+
+Rspec.describe Author do
+
+  it 'can create an author' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+    expect(charlotte_bronte.books).to eq([])
+  end
+
+  xit 'can write a book' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+
+    expect(jane_eyre.class).to be_a(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
+  end
+
+  xit 'can add more books' do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "1853")
+
+    expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+  end
+end
