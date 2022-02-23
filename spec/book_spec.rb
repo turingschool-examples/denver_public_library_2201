386b58ba487880aae31e98059db168102133cd4e
Alicia Watt <alicia.watt.aw@gmail.com>
2022-02-23 09:35:25 -0700

    feat: iteration 1 methods for author and publication_year

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -1,6 +1,6 @@
 require './lib/book'
 
-RSpec.descibe Book do
+RSpec.describe Book do
   let(:book) {
     Book.new({ author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird",
                publication_date: "July 11, 1960" })
@@ -10,8 +10,7 @@ RSpec.descibe Book do
   end
 
   it 'has attributes' do
-    expect(book.author_first_name).to eq("Harper")
-    expect(book.author_last_name).to eq("Lee")
+    expect(book.author).to eq("Harper Lee")
     expect(book.title).to eq("To Kill a Mockingbird")
     expect(book.publication_year).to eq("1960")
   end
