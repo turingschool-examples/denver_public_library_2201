0635820f357b95823051c6f5899f12fe3a8e12c5
Rin Fabian <pfabianschool@gmail.com>
2022-02-23 11:51:07 -0600

    test; added tests related to checking out books to library_spec

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -41,4 +41,68 @@ RSpec.describe Library do
     expect(dpl.publication_time_frame_for(charlotte_bronte)).to eq({start: "1847", end: "1857"})
     expect(dpl.publication_time_frame_for(harper_lee)).to eq({start: "1960", end: "1960"})
   end
+
+  xit 'can checkout books' do
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    expect(dpl.checkout(mockingbird)).to eq(false)
+    expect(dpl.checkout(jane_eyre)).to eq(false)
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.checkout(jane_eyre)).to eq(true)
+  end
+
+  xit 'can checkout a book only once' do
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    dpl.checkout(jane_eyre)
+
+    expect(dpl.checked_out_books).to eq([jane_eyre])
+    expect(dpl.checkout(jane_eyre)).to eq(false)
+
+    dpl.return(jane_eyre)
+    expect(dpl.checked_out_books).to eq([])
+
+    dpl.checkout(jane_eyre)
+    dpl.checkout(villette)
+    expect(dpl.checked_out_books).to eq([jane_eyre, villette])
+  end
+
+  xit 'can find the most popular book' do
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    dpl.checkout(jane_eyre)
+    dpl.checkout(villette)
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+
+    expect(dpl.most_popular_book).to eq([mockingbird])
+
+  end
 end
