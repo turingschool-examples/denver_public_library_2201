7b33b35ccb44383d1af0973d6d819fcb1617125b
Kim G <xiaole.guo@yahoo.com>
2022-02-23 10:22:44 -0700

    write and pass publication time frame test

--- lib/library.rb
+++ lib/library.rb
@@ -8,8 +8,17 @@ attr_reader :name, :books, :authors
 
     def add_author(author)
       @authors << author
-      @authors.each {|author| author.books.each {|book| @books << book if !@books.include?(book)}}
       # require "pry"; binding.pry
+      add_books
     end
 
+    def add_books
+    @authors.each {|author| author.books.each {|book| @books << book if !@books.include?(book)}}
+    end
+
+    def publication_time_frame_for(author)
+
+      years_of_pub = author.books.map {|book| book.publication_year}
+      publication_time_frame = {start: years_of_pub.min, end: years_of_pub.max}
+    end
 end
