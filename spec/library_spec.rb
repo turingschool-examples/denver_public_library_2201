820858520c7eba2c886e7b7255501c2a7d9edbc7
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-23 10:37:52 -0700

    Test: add tests for .checkout method

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -51,4 +51,45 @@ describe Library do
       expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start=>"1960", :end=>"1960"})
     end
   end
+
+  describe "checkout" do
+    it 'returns true if the book is in the library' do
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      professor = charlotte_bronte.write("The Professor", "1857")
+      villette = charlotte_bronte.write("Villette", "1853")
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+      expect(dpl.checkout(mockingbird)).to eq(false)
+      expect(dpl.checkout(jane_eyre)).to eq(false)
+
+      dpl.add_author(charlotte_bronte)
+      dpl.add_author(harper_lee)
+
+      expect(dpl.checkout(jane_eyre)).to be true
+    end
+
+    it 'adds the book to checked_out_books' do
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      professor = charlotte_bronte.write("The Professor", "1857")
+      villette = charlotte_bronte.write("Villette", "1853")
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+      dpl.add_author(charlotte_bronte)
+      dpl.add_author(harper_lee)
+      dpl.checkout(jayne_eyre)
+
+      expect(dpl.checked_out_books).to eq([jayne_eyre])
+    end
+
+    it 'can not checkout a book if it is already checked out' do
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      professor = charlotte_bronte.write("The Professor", "1857")
+      villette = charlotte_bronte.write("Villette", "1853")
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+      dpl.add_author(charlotte_bronte)
+      dpl.add_author(harper_lee)
+      dpl.checkout(jayne_eyre)
+
+      expect(dpl.checkout(jayne_eyre)).to eq(false)
+    end
+  end
 end
