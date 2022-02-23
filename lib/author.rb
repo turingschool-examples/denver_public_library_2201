324a689205255db094c2ed42a794c67b8fa09a6f
Kim G <xiaole.guo@yahoo.com>
2022-02-23 09:51:47 -0700

    write can write multi books test

--- lib/author.rb
+++ lib/author.rb
@@ -12,5 +12,6 @@ attr_reader :name, :books, :first_name, :last_name
 
   def write(title, publication_date)
     book = Book.new({author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: publication_date})
+    
   end
 end
