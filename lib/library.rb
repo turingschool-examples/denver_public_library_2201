f977a517e6184c9dc9b60c947ab36e4c05c1d78b
Alex Psitos <apsitos08@gmail.com>
2022-02-23 13:00:18 -0500

    Final commit

--- lib/library.rb
+++ lib/library.rb
@@ -1,8 +1,23 @@
 require './lib/author'
 
 class Library
-  attr_reader :name
+  attr_reader :name, :books, :authors
+  attr_accessor :author, :book
   def initialize(name)
     @name = name
+    @books = []
+    @authors = []
   end
-end 
+
+  def add_author(author)
+    @authors << author
+  end
+
+  def add_books
+    @authors.map do |author|
+    author.books
+      @books << book
+      
+    end
+  end
+end
--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -11,6 +11,40 @@ RSpec.describe Library do
   it "has readable attributes" do
   dpl = Library.new("Denver Public Library")
   expect(dpl.name).to eq("Denver Public Library")
-  end 
+  end
+
+  it "starts with empty arrays of books and authors" do
+  dpl = Library.new("Denver Public Library")
+  expect(dpl.books).to eq([])
+  expect(dpl.authors).to eq([])
+  end
+
+  it "can add authors to the authors array" do
+
+  dpl = Library.new("Denver Public Library")
+  charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+  jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+  professor = charlotte_bronte.write("The Professor", "1857")
+  villette = charlotte_bronte.write("Villette", "1853")
+  harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+  mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+  dpl.add_author(charlotte_bronte)
+  dpl.add_author(harper_lee)
+  expect(dpl.authors).to eq([charlotte_bronte, harper_lee])
+  end
+
+  it "adds the books written by authors to the books array" do
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    dpl.add_books
+    expect(dpl.books).to eq([charlotte_bronte, jane_eyre, professor, villette, mockingbird])
+    end
 
 end
