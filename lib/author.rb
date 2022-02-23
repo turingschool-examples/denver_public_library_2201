24616719a7c1c2d5cd5727da9bd014ee8ff6c923
Kim G <xiaole.guo@yahoo.com>
2022-02-23 09:52:16 -0700

    pass can write multi books test

--- lib/author.rb
+++ lib/author.rb
@@ -12,6 +12,7 @@ attr_reader :name, :books, :first_name, :last_name
 
   def write(title, publication_date)
     book = Book.new({author_first_name: @first_name, author_last_name: @last_name, title: title, publication_date: publication_date})
-    
+    @books << book
+    book
   end
 end
