17d7f8d752517a196177aa413d14181bc6deaf0c
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 10:41:33 -0700

    Feat: most popular book can be identified

--- lib/library.rb
+++ lib/library.rb
@@ -3,13 +3,14 @@ require './lib/author'
 require 'pry'
 
 class Library
-  attr_reader :name, :books, :authors, :checked_out_books
+  attr_reader :name, :books, :authors, :checked_out_books, :book_popularity
 
-  def initialize(name, books = [], authors = [], checked_out_books = [])
+  def initialize(name, books = [], authors = [], checked_out_books = [], book_popularity = Hash.new(0))
     @name = name
     @books = books
     @authors = authors
     @checked_out_books = checked_out_books
+    @book_popularity = book_popularity
   end
 
   def add_author(author)
@@ -38,12 +39,25 @@ class Library
       false
     else
       @checked_out_books << book
+      @book_popularity[book] += 1
       true
     end
   end
 
   def return(book)
     @checked_out_books.delete(book)
+  end
+
+  def most_popular_book
+    most_popular_book = nil
+
+    checked_out_amounts_array = @book_popularity.map { |book, num_checked_out| num_checked_out }
+    
+    @book_popularity.each do |book, num_checked_out|
+      most_popular_book = book if num_checked_out == checked_out_amounts_array.max
+    end
+
+    most_popular_book
   end
 
 end
