0a4ee40c146531e4e0495d975357703455e1e82b
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 09:19:54 -0700

    Test: write initialize test for author class

---
+++ spec/author_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+require './lib/book'
+require './lib/author'
+
+RSpec.describe Author do
+  it "exists" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    expect(charlotte_bronte).to be_an_instance_of(Author)
+  end
+end
