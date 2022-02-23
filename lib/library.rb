dad156cd2019daf21a14395672385d7cf4f4ed69
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-23 10:30:15 -0700

    Feat: .publication_time_frame_for method passes tests

--- lib/library.rb
+++ lib/library.rb
@@ -14,5 +14,20 @@ class Library
       @books << book
     end
   end
-  
+
+  def publication_time_frame_for(desired_author)
+    years = []
+    time_frame = {:start => nil, :end => nil}
+    @authors.each do |author|
+      if author.name == desired_author.name
+        author.books.each do |book|
+          years << book.publication_year.to_i
+        end
+      end
+    end
+    time_frame[:start] = years.min.to_s
+    time_frame[:end] = years.max.to_s
+    return time_frame
+  end
+
 end
