a30e7e19067e2a99604c47d947456c0da7431308
Kim G <xiaole.guo@yahoo.com>
2022-02-23 09:58:34 -0700

    write and pass name, authors, books initiail tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -1,5 +1,6 @@
 require './lib/book'
 require './lib/author'
+require './lib/Library'
 
 RSpec.describe Library do
   before(:each) do
@@ -9,3 +10,16 @@ RSpec.describe Library do
     it "exists" do
       expect(@dpl).to be_an_instance_of(Library)
     end
+
+    it "has a name" do
+      expect(@dpl.name).to eq("Denver Public Library")
+    end
+
+    it "has no books when initialized" do
+      expect(@dpl.books).to eq([])
+    end
+
+    it "has no authors listed when initialized" do
+      expect(@dpl.authors).to eq([])
+    end
+end
