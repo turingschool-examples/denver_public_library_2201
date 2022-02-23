7823638b95f68c8f5f6e1c08945ad4c04e261076
Andrew Speth <speth.andrew@yahoo.com>
2022-02-23 10:04:51 -0700

    tests: Library can checkout/return books and find most popular

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -43,4 +43,69 @@ RSpec.describe Library do
     expect(dpl.publication_time_frame_for(charlotte_bronte)).to eq({:start => "1847", :end => "1857"})
     expect(dpl.publication_time_frame_for(harper_lee)).to eq({:start => "1960", :end => "1960"})
   end
+
+  it "can checkout and return books" do
+    dpl = Library.new("Denver Public Library")
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.checkout(mockingbird)).to be false
+    expect(dpl.checkout(jane_eyre)).to be false
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    expect(dpl.checkout(jane_eyre)).to be true
+    expect(dpl.checked_out_books).to eq([jane_eyre])
+    expect(dpl.checkout(jane_eyre)).to be false
+
+    dpl.return(jane_eyre)
+    expect(dpl.checked_out_books).to eq([])
+    expect(dpl.checkout(jane_eyre)).to be true
+    expect(dpl.checkout(villette)).to be true
+    expect(dpl.checked_out_books).to eq([jane_eyre, villette])
+  end
+
+  xit "can find the most popular book" do
+    dpl = Library.new("Denver Public Library")
+
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+
+    expect(dpl.checkout(mockingbird)).to be false
+    expect(dpl.checkout(jane_eyre)).to be false
+
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    expect(dpl.checkout(jane_eyre)).to be true
+    expect(dpl.checked_out_books).to eq([jane_eyre])
+    expect(dpl.checkout(jane_eyre)).to be false
+
+    dpl.return(jane_eyre)
+    expect(dpl.checked_out_books).to eq([])
+    expect(dpl.checkout(jane_eyre)).to be true
+    expect(dpl.checkout(villette)).to be true
+    expect(dpl.checked_out_books).to eq([jane_eyre, villette])
+
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+    dpl.checkout(mockingbird)
+    expect(dpl.most_popular_book).to eq([mockingbird])
+  end
 end
