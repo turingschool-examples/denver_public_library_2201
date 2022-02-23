2003db10224d9a4eb65ce57e8071e24e9421b964
Andrew Speth <speth.andrew@yahoo.com>
2022-02-23 09:26:01 -0700

    feat: Author can write books

--- lib/author.rb
+++ lib/author.rb
@@ -1,6 +1,6 @@
 class Author
   attr_reader :books
-  
+
   def initialize(attributes)
     @first_name = attributes[:first_name]
     @last_name = attributes[:last_name]
@@ -9,5 +9,11 @@ class Author
 
   def name
     "#{@first_name} #{@last_name}"
+  end
+
+  def write(title, publication_date)
+    book = Book.new({author_first_name: "#{@first_name}", author_last_name: "#{@last_name}", title: title, publication_date: publication_date})
+    books << book
+    return book
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -8,4 +8,15 @@ RSpec.describe Author do
     expect(charlotte_bronte.name).to eq("Charlotte Bronte")
     expect(charlotte_bronte.books).to eq([])
   end
+
+  it "can write books" do
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    expect(jane_eyre.class).to eq(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
+
+    villette = charlotte_bronte.write("Villette", "1853")
+    expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+  end
 end
