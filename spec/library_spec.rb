2ddea411de7e9eb89ad9f4b4000868d37afda3bb
James Harkins <jh.harkins@protonmail.com>
2022-02-23 10:19:04 -0700

    test: create test for checkout method

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -42,4 +42,11 @@ RSpec.describe Library do
       expect(@dpl.publication_time_frame_for(@harper_lee)).to eq({:start => "1960", :end => "1960"})
     end
   end
+
+  describe '#checkout' do
+    it 'can check out books' do
+      expect(@dpl.checkout(@jane_eyre)).to eq(true)
+      expect(@dpl.checkout(@jane_eyre)).to eq(false)
+    end
+  end
 end
