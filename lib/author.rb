5f6a8d6190c097d0f569cf85da8846d20546c78a
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 09:37:35 -0700

    Feat: pass 'creates instance of Book class' test

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,21 @@
+require './lib/book'
+require 'pry'
+
 class Author
-  attr_reader :author_info
-  
-  def initialize(author_info = {})
+  attr_reader :author_info, :books
+
+  def initialize(author_info = {}, books = [])
     @author_info = author_info
+    @books = books
   end
+
+  def name
+    "#{@author_info[:first_name]} #{@author_info[:last_name]}"
+  end
+
+  def write(title, pub_date)
+    @books << Book.new(title: title, publication_date: pub_date)
+    Book.new(title: title, publication_date: pub_date)
+  end
+  binding.pry
 end
