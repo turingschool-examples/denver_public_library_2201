6459ac05c1430ce68d9481110cfb158620addf39
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 09:04:39 -0700

    Test: write initialize test for book class

---
+++ spec/book_spec.rb
@@ -0,0 +1,10 @@
+require 'rspec'
+require './lib/book'
+
+RSpec.describe Book do
+  it "exists" do
+    book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+
+    expect(book).to be_an_instance_of(Book)
+  end
+end
