284acf2d21707c47c616e6aae81b965b2bf37bbe
Chris Romero <chrisleeromero1@gmail.com>
2022-02-23 09:12:22 -0700

    Test: add attribute tests for book class

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,9 @@
 class Book
-
+attr_reader :title, :author, :publication_year
   def initialize(attributes)
-    
+    # @title = attributes[:title]
+    # @author = attributes[:author]
+    # @publication_year = attributes[:publication_year]
   end
 
 end
--- spec/book.spec.rb
+++ spec/book.spec.rb
@@ -9,6 +9,21 @@ RSpec.describe Book do
       expect(book).to be_an_instance_of(Book)
     end
 
+    it "has a title" do
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+      expect(book.title).to eq("To Kill a Mockingbird")
+    end
+
+    xit "has a author" do
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+      expect(book.author).to eq(author_last_name)
+    end
+
+    xit "has a publication_year" do
+      book = Book.new({author_first_name: "Harper", author_last_name: "Lee", title: "To Kill a Mockingbird", publication_date: "July 11, 1960"})
+      expect(book.publication_year).to eq("1960")
+    end
+
   end
 
 
