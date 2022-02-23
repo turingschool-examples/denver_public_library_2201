2d53801f6c3dfe43cc382908a7de70c231a19350
Chris Romero <chrisleeromero1@gmail.com>
2022-02-23 09:39:32 -0700

    Feat: add attributes tests and passing methods for author class

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,9 @@
 class Author
+attr_reader :name, :books
 
-def initialize(attributes)
-  @attributes = attributes
-end
+  def initialize(attributes)
+    @name = attributes[:first_name] + " " + attributes[:last_name]
+    @books = []
+  end
 
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -7,8 +7,18 @@ RSpec.describe Author do
   describe "Iteration 2" do
 
     it "exists" do
-      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})   
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
       expect(charlotte_bronte).to be_an_instance_of(Author)
+    end
+
+    it "has a name" do
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+    end
+
+    it "has zero books by default" do
+      charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+      expect(charlotte_bronte.books).to eq([])
     end
 
   end
