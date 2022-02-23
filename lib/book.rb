1c9fd7a5574c4cfbf217ad2f7adccb060cf2aabb
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 09:17:36 -0700

    Feat: pass book method test

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,21 @@
 
 
 class Book
+  attr_reader :book_info
 
   def initialize(book_info = {})
     @book_info = book_info
+  end
+
+  def title
+    @book_info[:title]
+  end
+
+  def author
+    "#{@book_info[:author_first_name]} #{@book_info[:author_last_name]}"
+  end
+
+  def publication_year
+    @book_info[:publication_date].split.last
   end
 end
