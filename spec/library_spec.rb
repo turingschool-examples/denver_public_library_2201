b970243c4e69644935ae08f3106dde048822236d
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-23 10:10:48 -0700

    Feat: .add_author passes tests

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -5,10 +5,6 @@ describe Library do
   dpl = Library.new("Denver Public Library")
   charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
   harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
-  jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
-  professor = charlotte_bronte.write("The Professor", "1857")
-  villette = charlotte_bronte.write("Villette", "1853")
-  mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
 
   describe 'initialization' do
     it 'has a name' do
@@ -25,14 +21,19 @@ describe Library do
   end
 
   describe 'add_author' do
-    dpl.add_author(charlotte_bronte)
-    dpl.add_author(harper_lee)
-
     it 'adds the given Author to @authors' do
+      dpl.add_author(charlotte_bronte)
+      dpl.add_author(harper_lee)
       expect(dpl.authors).to eq([charlotte_bronte, harper_lee])
     end
 
     it "adds all of the author's @books to the library's @books" do
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      professor = charlotte_bronte.write("The Professor", "1857")
+      villette = charlotte_bronte.write("Villette", "1853")
+      mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+      dpl.add_author(charlotte_bronte)
+      dpl.add_author(harper_lee)
       expect(dpl.books).to eq([jane_eyre, professor, villette, mockingbird])
     end
   end
