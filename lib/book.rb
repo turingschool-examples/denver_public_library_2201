f5dfce160f0b3e250c9c239484571ee8b38acdb4
Scott Sullivan <scottsullivanltd@gmail.com>
2022-02-23 09:29:39 -0700

    Initial commit

---
+++ lib/book.rb
@@ -0,0 +1,18 @@
+class Book
+  attr_reader :author_first_name, :author_last_name, :title, :publication_date
+
+  def initialize(attributes)
+    @author_first_name = attributes[:author_first_name]
+    @author_last_name = attributes[:author_last_name]
+    @title = attributes[:title]
+    @publication_date = attributes[:publication_date]
+  end
+
+  def author
+    "#{author_first_name} #{ author_last_name}"
+  end
+
+  def publication_year
+    "1" + publication_date.delete('July 11, ')
+  end
+end
