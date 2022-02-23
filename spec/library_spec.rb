e287422b90efaa31abf73e98c02ca3aed6d7ec45
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 09:48:56 -0700

    Test: test library class for holding empty arrays of books and authors

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -9,4 +9,11 @@ RSpec.describe Library do
 
     expect(dpl).to be_an_instance_of(Library)
   end
+
+  it "holds empty arrays of books and authors by default" do
+    dpl = Library.new("Denver Public Library")
+
+    expect(dpl.books).to eq([])
+    expect(dpl.authors).to eq([])
+  end
 end
