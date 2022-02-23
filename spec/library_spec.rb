617ffe4409e85740452f32973ec30074e207d1e9
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 10:27:57 -0700

    Test: most popular book can be identified

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -104,9 +104,32 @@ RSpec.describe Library do
     dpl.add_author(harper_lee)
     dpl.checkout(jane_eyre)
     dpl.checkout(mockingbird)
-    
+
     dpl.return(jane_eyre)
 
     expect(dpl.checked_out_books).to eq([mockingbird])
+  end
+
+  it "can identify the most popular book)" do
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    dpl.checkout(jane_eyre)
+    dpl.return(jane_eyre)
+    dpl.checkout(jane_eyre)
+    dpl.checkout(villette)
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+    dpl.checkout(mockingbird)
+    dpl.return(mockingbird)
+    dpl.checkout(mockingbird)
+
+    expect(dpl.most_popular_book).to eq(mockingbird)
   end
 end
