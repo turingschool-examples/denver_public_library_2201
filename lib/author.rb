0f1036fed6c0bc336a109b2eddc50f523065c77a
Sandisz Thieme <sandiszthieme@gmail.com>
2022-02-23 09:28:34 -0700

    Give Author class ability to write books

--- lib/author.rb
+++ lib/author.rb
@@ -1,7 +1,23 @@
 class Author
+  attr_reader :books
   def initialize(info)
     @first_name = info[:first_name]
     @last_name = info[:last_name]
     @books = []
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def write(title, pub_date)
+    book = Book.new({
+      author_first_name: @first_name,
+      author_last_name: @last_name,
+      title: title,
+      publication_date: pub_date
+      })
+    books << book
+    book
   end
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -1,10 +1,22 @@
 require './lib/book'
 require './lib/author'
+require 'pry'
 
 describe Author do
   charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
 
   it "Exists" do
     expect(charlotte_bronte).to be_an_instance_of(Author)
+  end
+
+  it "has a name" do
+    expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+  end
+
+  it "can write books" do
+    expect(charlotte_bronte.books).to eq([])
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    expect(jane_eyre.class).to eq(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
   end
 end
