f27370cbd6ac3eaa633a4445e413157edf7901e7
Zachary Imbriaco <zachary@imbriaco.com>
2022-02-23 12:01:17 -0500

    Complete iteration 3.

--- lib/author.rb
+++ lib/author.rb
@@ -1,20 +1,19 @@
 require './lib/book'
 
 class Author
-  attr_reader :first_name, :last_name, :books
+  attr_reader :name, :books
 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
+  def initialize(name)
+    @name = name
     @books = []
   end
 
   def name
-    return "#{first_name} #{last_name}"
+    return @name[:first_name] + " " + @name[:last_name]
   end
 
   def write(title, publication_date)
-    new_book = Book.new({author_first_name: first_name, author_last_name: last_name, title: title, publication_date: publication_date})
+    new_book = Book.new({author_first_name: @name[:first_name], author_last_name: @name[:last_name], title: title, publication_date: publication_date})
     books << new_book
     return new_book
   end
--- lib/library.rb
+++ lib/library.rb
@@ -8,4 +8,19 @@ class Library
     @books = []
     @authors = []
   end
+
+  def add_author(author)
+    authors << author
+    author.books.each do |book|
+      books << book
+    end
+  end
+  
+  def publication_time_frame_for(author)
+    pub_years = []
+    author.books.each do |book|
+      pub_years << book.publication_year.to_i
+    end
+    return {:start => pub_years.min.to_s, :end => pub_years.max.to_s}
+  end
 end
