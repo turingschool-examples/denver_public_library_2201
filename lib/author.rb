53a3d70f6544b68bef0abfd812b6e4f712823340
Kim G <xiaole.guo@yahoo.com>
2022-02-23 09:19:42 -0700

    pass name test

--- lib/author.rb
+++ lib/author.rb
@@ -2,7 +2,7 @@ class Author
 
 attr_reader :name
   def initialize(author)
-
+    @name = author[:first_name] + " " + author[:last_name]
   end
 
 end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,16 +3,16 @@ require './lib/author'
 
 RSpec.describe Author do
   before(:each) do
-
     @charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
   end
+
   it "exists" do
     expect(@charlotte_bronte).to be_an_instance_of(Author)
   end
 
   it "has a name" do
-    expect(@charlotte_bronte.name).to eq["Charlotte Bronte"]
+    expect(@charlotte_bronte.name).to eq("Charlotte Bronte")
   end
 
-  
+
 end
