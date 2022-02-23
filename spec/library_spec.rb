2738b3798c53b3190872ccf63efbdce75877ed0e
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 10:16:46 -0700

    Test: test that books can't be checked out twice

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -76,4 +76,19 @@ RSpec.describe Library do
     expect(dpl.checkout(jane_eyre)).to eq(true)
     expect(dpl.checked_out_books).to eq([jane_eyre])
   end
+
+  it "won't checkout book if it alrady exists in checked_out_books array" do
+    dpl = Library.new("Denver Public Library")
+    charlotte_bronte = Author.new({first_name: "Charlotte", last_name: "Bronte"})
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    professor = charlotte_bronte.write("The Professor", "1857")
+    villette = charlotte_bronte.write("Villette", "1853")
+    harper_lee = Author.new({first_name: "Harper", last_name: "Lee"})
+    mockingbird = harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+    dpl.add_author(charlotte_bronte)
+    dpl.add_author(harper_lee)
+    dpl.checkout(jane_eyre)
+
+    expect(dpl.checkout(jane_eyre)).to eq(false)
+  end
 end
