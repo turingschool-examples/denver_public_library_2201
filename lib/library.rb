cfd8079f87b82f3fdf9e2b9ca46bc3d1ef05353b
Johnny Bowman <johntbowman.production@gmail.com>
2022-02-23 10:04:24 -0700

    Feat: complete publication_time_frame_for method

--- lib/library.rb
+++ lib/library.rb
@@ -4,10 +4,31 @@ require 'pry'
 
 class Library
   attr_reader :name, :books, :authors
-  
+
   def initialize(name, books = [], authors = [])
     @name = name
     @books = books
     @authors = authors
+  end
+
+  def add_author(author)
+    @authors << author
+    author.books.each do |book|
+      @books << book
+    end
+  end
+
+  def publication_time_frame_for(author)
+    time_frame = {}
+    book_dates = []
+
+    author.books.each do |book|
+      book_dates << book.publication_year.to_i
+    end
+
+    time_frame[:start] = book_dates.minmax.first.to_s
+    time_frame[:end] = book_dates.minmax.last.to_s
+
+    time_frame
   end
 end
