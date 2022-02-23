88040f5f9cfcd1ce9d1eadfff7b84afc3d82076b
Brylan Gannon <brylan.gannon112@gmail.com>
2022-02-23 09:37:17 -0700

    Test: Write tests .write method

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -13,4 +13,17 @@ describe Author do
       expect(charlotte_bronte.books).to eq([])
     end
   end
+
+  describe "write" do
+    it 'creates a book class' do
+      jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+      expect(jane_eyre).to be_a(Book)
+      expect(jane_eyre.title).to eq("Jane Eyre")
+    end
+
+    it "adds the Book to the author's @books" do
+      villette = charlotte_bronte.write("Villette", "1853")
+      expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+    end
+  end
 end
