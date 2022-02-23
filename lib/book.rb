58ee386809db491851d0a014cfb8253bf57a5694
Kim G <xiaole.guo@yahoo.com>
2022-02-23 09:11:26 -0700

    passed author test

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,8 @@
 class Book
-  attr_reader :title
+  attr_reader :title, :author
   def initialize(book)
     @title = book[:title]
+    @author = book[:author_first_name] + " " + book[:author_last_name]
   end
 
 end
