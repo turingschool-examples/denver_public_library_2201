d492a589fd779823de4a4afe5060427f68f7aa6b
alex randolph <alexgeorgerandolph@gmail.com>
2022-02-23 09:56:14 -0700

    Feat: Tests and passing code for iteration 2

---
+++ lib/author.rb
@@ -0,0 +1,36 @@
+require './lib/book'
+
+class Author
+  attr_reader :books
+
+
+  def initialize(attributes)
+    @attributes = attributes
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @books = []
+  end
+
+  def name
+    "#{@first_name} #{@last_name}"
+  end
+
+  def write(book, date)
+    @book = book
+    @date = date
+    new_hash = {}
+    new_hash[:author_first_name] = @first_name
+    new_hash[:author_last_name] = @last_name
+    new_hash[:title] = @book
+    new_hash[:publication_date] = @date
+    @new_book = Book.new(new_hash)
+    @books << @new_book
+    @new_book
+
+  end
+
+
+
+
+
+end
--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -9,5 +9,26 @@ RSpec.describe Author do
     expect(charlotte_bronte).to be_an_instance_of(Author)
   end
 
+  it 'has readable #name' do
+    expect(charlotte_bronte.name).to eq("Charlotte Bronte")
+  end
+
+  it 'has empty #books array' do
+    expect(charlotte_bronte.books).to eq([])
+  end
+
+  it 'it can #writee a book' do
+    jane_eyre = charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = charlotte_bronte.write("Villette", "febuary 15, 1853")
+    expect(villette).to be_an_instance_of(Book)
+    expect(jane_eyre).to be_an_instance_of(Book)
+    expect(jane_eyre.title).to eq("Jane Eyre")
+    expect(jane_eyre.class).to eq(Book)
+
+    expect(charlotte_bronte.books).to eq([jane_eyre, villette])
+
+
+  end
+
 
 end
