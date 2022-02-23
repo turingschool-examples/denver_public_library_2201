9c528bd0e453a697abf21ce2f4764a8b2037d9b7
Andrew Speth <speth.andrew@yahoo.com>
2022-02-23 09:55:15 -0700

    feat: Library can find publication time frames

--- lib/library.rb
+++ lib/library.rb
@@ -12,4 +12,12 @@ class Library
     @books << author.books
     @books.flatten!
   end
+
+  def publication_time_frame_for(author)
+    books_by_year = author.books.sort_by {|book| book.publication_year}
+    time_frame = {}
+    time_frame[:start] = books_by_year.first.publication_year
+    time_frame[:end] = books_by_year.last.publication_year
+    time_frame
+  end
 end
